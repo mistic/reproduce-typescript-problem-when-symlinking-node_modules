f37b2d9c459550ac9278e1eab3aaa06b6887dbc8
Tiago Costa <tiagoffcc@hotmail.com>
2021-02-18 01:43:26 +0000

    chore(NA): make the error to actually happen by not using node_modules on the parent

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,12 +1,22 @@
 import React, { FC } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, RouteComponentProps, withRouter } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './styles/globalStyles';
+
+export type NavProps = RouteComponentProps;
+
+export const Nav = withRouter(({ history }: NavProps) => {
+  return (
+    <div
+    />
+  );
+});
 
 const App: FC = () => (
   <>
     <BrowserRouter>
       <Routes />
+      <Nav />
     </BrowserRouter>
     <GlobalStyle />
   </>
